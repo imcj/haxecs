7914e6ca4fb9eb5612c9a2de41ccd0ed18b4cfb2
cj <cj@imcj.me>
2014-02-27 12:59:43 +0800

    TODO的实例代码

---
+++ example/haxecs-rectangle/src/Main.hx
@@ -0,0 +1,20 @@
+package;
+
+
+import hx.xfl.openfl.Layer;
+import flash.display.Sprite;
+
+
+class Main extends Sprite
+{
+	public function new()
+	{
+		super();
+        trace("hello");
+		var document = hx.xfl.XFLDocument.open("assets/Rectangle");
+        trace(document);
+        var timeline = document.getTimeLine("Scene 1");
+        var layer = new Layer(timeline.getLayer("Layer 1"));
+        addChild(layer);		
+	}
+}
