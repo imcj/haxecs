980fe80405d2a223c01efcbebb32a5f833c87a9b
xclmwc <xclmwc@aliyun.com>
2014-03-20 16:47:51 +0800

    添加对曲线的填充处理
    对曲线的多余moveTo进行忽略处理
    交换填充和线条绘制顺序，先填充，后线条

--- src/hx/xfl/openfl/ShapeInstance.hx
+++ src/hx/xfl/openfl/ShapeInstance.hx
@@ -15,6 +15,47 @@ class ShapeInstance extends Shape
         this.dom = dom;
         var document = dom.frame.layer.timeLine.document;
 
+        //绘制填充
+        var prefill = null;
+        var preX = -1.0;
+        var preY = -1.0;
+        for (edge in dom.fillEdges) {
+            var fill = dom.fills.get(edge.fillStyle1);
+            if(edge.fillStyle0 !=0)fill = dom.fills.get(edge.fillStyle0);
+            if (null != fill) {
+                switch (fill.type) {
+                    case "SolidColor":
+                        if (fill != prefill) {
+                            this.graphics.endFill();
+                            this.graphics.beginFill(fill.color);
+                            preX = -1.0;
+                            preY = -1.0;
+                        }
+                        prefill = fill;
+                }
+            }
+            for (draw in edge.edges) {
+                if (draw.x == preX && draw.y == preY) 
+                    continue;
+                switch (draw.type) {
+                    case "moveTo":
+                        this.graphics.moveTo(draw.x, draw.y);
+                    case "lineTo":
+                        this.graphics.lineTo(draw.x, draw.y);
+                    case "curveTo":
+                        this.graphics.curveTo(draw.x, draw.y, draw.anchorX, draw.anchorY);
+                }
+                if ("curveTo" != draw.type) {
+                    preX = draw.x;
+                    preY = draw.y;
+                }else {
+                    preX = draw.anchorX;
+                    preY = draw.anchorY;
+                }
+            }
+        }
+        this.graphics.endFill();
+
         // 绘制线条
         for (edge in dom.edges) {
             var stroke = dom.strokes.get(edge.strokeStyle);
@@ -35,38 +76,6 @@ class ShapeInstance extends Shape
                     case "curveTo":
                         this.graphics.curveTo(draw.x, draw.y, draw.anchorX, draw.anchorY);
                 }
-            }
-        }
-
-        //绘制填充
-        var prefill = null;
-        var preX = -1.0;
-        var preY = -1.0;
-        for (edge in dom.fillEdges) {
-            var fill = dom.fills.get(edge.fillStyle1);
-            if(edge.fillStyle0 !=0)fill = dom.fills.get(edge.fillStyle0);
-            if (null != fill) {
-                switch (fill.type) {
-                    case "SolidColor":
-                        if (fill != prefill) {
-                            this.graphics.beginFill(fill.color);
-                        }
-                        prefill = fill;
-                }
-            }
-            for (draw in edge.edges) {
-                if (draw.x == preX && draw.y == preY) 
-                    continue;
-                switch (draw.type) {
-                    case "moveTo":
-                        this.graphics.moveTo(draw.x, draw.y);
-                    case "lineTo":
-                        this.graphics.lineTo(draw.x, draw.y);
-                    case "curveTo":
-                        this.graphics.curveTo(draw.x, draw.y, draw.anchorX, draw.anchorY);
-                }
-                preX = draw.x;
-                preY = draw.y;
             }
         }
     }
