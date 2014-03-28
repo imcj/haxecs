ae995ad7fd4626ed293deae5dbbe497c3c3f7684
xclmwc <xclmwc@aliyun.com>
2014-03-28 16:21:28 +0800

    修复Frame解析错误

--- src/hx/xfl/assembler/DOMFrameAssembler.hx
+++ src/hx/xfl/assembler/DOMFrameAssembler.hx
@@ -34,17 +34,23 @@ class DOMFrameAssembler extends XFLBaseAssembler
             fillProperty(frame, element);
             frames.push(frame);
 
-            for (dom_element in element.firstElement().elements()) {
-                if ("AnimationCore" == dom_element.nodeName) {
-                    var animation_assembler = new DOMAnimationCoreAssembler(document);
-                    frame.animation = animation_assembler.parse(dom_element);
+            for (element in element.elements()) {
+                if ("motionObjectXML" == element.nodeName) {
+                    for (ani_element in element.elements()) {
+                        var animation_assembler = new DOMAnimationCoreAssembler(document);
+                        frame.animation = animation_assembler.parse(ani_element);
+                    }
+                    
                 }else {
-                    assembler = assemblers.get(dom_element.nodeName);
-                    if (null == assembler)
-                        continue;
-                        // throw dom_element.nodeName + 
-                        //       " is not support element type.";
-                    frame.addElement(assembler.parse(dom_element));
+                    for (dom_element in element.elements()) {
+                        assembler = assemblers.get(dom_element.nodeName);
+                        if (null == assembler)
+                            continue;
+                            // throw dom_element.nodeName + 
+                            //       " is not support element type.";
+                        frame.addElement(assembler.parse(dom_element));
+                    }
+                    
                 }
             }
         }
