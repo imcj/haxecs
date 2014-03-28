aa1cb2c132a2964fca3edb1f97363af4e8673c83
xclmwc <xclmwc@aliyun.com>
2014-03-28 15:13:34 +0800

    添加KeyFrame解析

--- src/hx/xfl/motion/KeyFrame.hx
+++ src/hx/xfl/motion/KeyFrame.hx
@@ -1,5 +1,7 @@
 package hx.xfl.motion;
+
 import flash.geom.Point;
+import hx.xfl.assembler.XFLBaseAssembler;
 
 class KeyFrame
 {
@@ -18,6 +20,18 @@ class KeyFrame
 
     public function parse(data:Xml)
     {
-        
+        var assembler = new XFLBaseAssembler(null);
+        assembler.fillProperty(this, data, ["anchor", "next", "previous"]);
+        this.anchor = parsePoint(data.get("anchor"));
+        this.next = parsePoint(data.get("next"));
+        this.previous = parsePoint(data.get("previous"));
+    }
+
+    function parsePoint(str:String):Point
+    {
+        var arrStr = str.split(",");
+        var px = Std.parseFloat(arrStr[0]);
+        var py = Std.parseFloat(arrStr[1]);
+        return new Point(px, py);
     }
 }
