8a37106f8141b193ed1f53f4b966d8da15ef60c9
xclmwc <xclmwc@aliyun.com>
2014-03-28 16:11:23 +0800

    添加获得当前帧前后关键帧方法

--- src/hx/xfl/motion/Property.hx
+++ src/hx/xfl/motion/Property.hx
@@ -15,6 +15,24 @@ class Property
         keyFrames = [];
     }
 
+    public function getStarEnd(frameIndex:Int):Array<KeyFrame>
+    {
+        var starEnd:Array<KeyFrame> = [];
+        for (kf in keyFrames) {
+            var keyIndex = Std.int(kf.timevalue / 1000);
+            if (keyIndex == frameIndex) {
+                starEnd.push(kf);
+                break;
+            }else if (keyIndex < frameIndex) {
+                starEnd.push(kf);
+            }else if (keyIndex > frameIndex) {
+                starEnd.push(kf);
+                break;
+            }
+        }
+        return starEnd;
+    }
+
     public function parse(data:Xml)
     {
         var assembler = new XFLBaseAssembler(null);
