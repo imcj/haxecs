3bfaad10c4489ea78db3fe2224d98846d1e82933
xclmwc <xclmwc@aliyun.com>
2014-04-02 17:01:10 +0800

    添加辅助类

--- src/hx/xfl/openfl/MotionObject.hx
+++ src/hx/xfl/openfl/MotionObject.hx
@@ -2,6 +2,8 @@ package hx.xfl.openfl;
 import flash.display.DisplayObject;
 import hx.geom.Matrix;
 import hx.xfl.DOMAnimationCore;
+import hx.xfl.motion.Property;
+import hx.xfl.motion.PropertyContainer;
 
 class MotionObject
 {
@@ -15,6 +17,29 @@ class MotionObject
     public function getMatrix(frame:Int):Matrix 
     {
         var matrix = new Matrix();
+        
         return matrix;
+    }
+
+    public function getContainers(name:String):Map<String, PropertyContainer>
+    {
+        var head = dom.PropertyContainers.get(name);
+        if (null == container) return null;
+        var containers = head.children;
+        return containers;
+    }
+
+    public function getProperty(name:String):Property 
+    {
+        for (up in dom.PropertyContainers) {
+            for (container in up.children) {
+                for (property in container.children) {
+                    if (name == property.id) {
+                        return property;
+                    }
+                }
+            }
+        }
+        return null;
     }
 }
