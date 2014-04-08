799deb686941f4072fc1c915dd56f599124250b0
xclmwc <xclmwc@aliyun.com>
2014-04-08 14:18:44 +0800

    添加对象以及对象子对象是否可访问属性

--- src/hx/xfl/DOMSymbolInstance.hx
+++ src/hx/xfl/DOMSymbolInstance.hx
@@ -7,6 +7,8 @@ class DOMSymbolInstance extends DOMInstance
     public var selected:Bool;
     public var symbolType:String;
     public var loop:String;
+    public var silent:Bool;
+    public var hasAccessibleData:Bool;
 
     public function new()
     {
@@ -15,5 +17,7 @@ class DOMSymbolInstance extends DOMInstance
         selected = false;
         symbolType = "";
         loop = "";
+        silent = false;
+        hasAccessibleData = false;
     }
 }
