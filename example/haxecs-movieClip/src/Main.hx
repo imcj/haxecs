8d769765d630a36e3c65886dc22bc97260e2314b
xclmwc <xclmwc@aliyun.com>
2014-04-16 10:37:17 +0800

    修改Movieclip事例代码

--- example/haxecs-movieClip/src/Main.hx
+++ example/haxecs-movieClip/src/Main.hx
@@ -25,6 +25,9 @@ class Main extends Sprite
         bulidButton("播放", 100, play);
         bulidButton("停止", 200, stop);
         bulidButton("下一场景", 300, nextScene);
+        bulidButton("跳到场景1播放", 400, toScene1);
+        bulidButton("跳到场景2播放", 500, toScene2);
+        bulidButton("上一帧", 600, prevFrame);
     }
 
     function bulidButton(name:String,x:Float,fun:MouseEvent->Void):Void 
@@ -37,6 +40,16 @@ class Main extends Sprite
         btn.addEventListener(MouseEvent.CLICK, fun);
     }
 
+    function toScene2(e:MouseEvent):Void
+    {
+        mv.gotoAndPlay(0, "场景 2");
+    }
+
+    function toScene1(e:MouseEvent):Void
+    {
+        mv.gotoAndPlay(0, "Scene 1");
+    }
+
     function nextScene(e:MouseEvent):Void 
     {
         mv.nextScene();
@@ -46,14 +59,19 @@ class Main extends Sprite
     {
         mv.play();
     }
-    
+
     function stop(e:MouseEvent):Void
     {
         mv.stop();
     }
-    
+
     function nextFrame(e:MouseEvent):Void
     {
         mv.nextFrame();
+    }
+
+    function prevFrame(e:MouseEvent):Void
+    {
+        mv.prevFrame();
     }
 }
