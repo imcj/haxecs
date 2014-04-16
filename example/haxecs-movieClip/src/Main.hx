8d1ae2096a54430f61df5132a66fee2ba46ccd05
xclmwc <xclmwc@aliyun.com>
2014-04-16 10:10:36 +0800

    添加设置按钮函数

--- example/haxecs-movieClip/src/Main.hx
+++ example/haxecs-movieClip/src/Main.hx
@@ -21,16 +21,37 @@ class Main extends Sprite
         mv = new MovieClip(document.timeLines);
         addChild(mv);
 
-        var textField = new TextField();
-        textField.text = "下一帧";
-        addChild(textField);
-        textField.addEventListener(MouseEvent.CLICK, nextFrame);
+        var tfNextFrame = new TextField();
+        tfNextFrame.text = "下一帧";
+        tfNextFrame.selectable = false;
+        addChild(tfNextFrame);
+        tfNextFrame.addEventListener(MouseEvent.CLICK, nextFrame);
 
         var tfStop = new TextField();
         tfStop.text = "停止";
         tfStop.x = 100;
         addChild(tfStop);
         tfStop.addEventListener(MouseEvent.CLICK, stop);
+
+        var tfPlay = new TextField();
+        tfPlay.text = "播放";
+        tfPlay.x = 200;
+        addChild(tfPlay);
+        tfPlay.addEventListener(MouseEvent.CLICK, play);
+    }
+
+    function setButton(btn:TextField,name:String,x:Float,fun:MouseEvent->Void):Void 
+    {
+        btn.text = name;
+        btn.selectable = false;
+        btn.x = x;
+        addChild(btn);
+        btn.addEventListener(MouseEvent.CLICK, fun);
+    }
+
+    function play(e:MouseEvent):Void
+    {
+        mv.play();
     }
     
     function stop(e:MouseEvent):Void
