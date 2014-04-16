be300566b415611971af50d900623e4888247d83
xclmwc <xclmwc@aliyun.com>
2014-04-16 09:45:26 +0800

    修改主函数

--- example/haxecs-movieClip/src/Main.hx
+++ example/haxecs-movieClip/src/Main.hx
@@ -1,20 +1,45 @@
 package;
 
+import flash.events.MouseEvent;
+import flash.Lib;
+import flash.text.TextField;
+import haxe.Timer;
 import hx.xfl.openfl.display.MovieClip;
 import flash.display.Sprite;
 
 
 class Main extends Sprite
 {
+    var mv:MovieClip;
+
     public function new()
     {
         super();
         trace("hello");
         var document = hx.xfl.XFLDocument.open("assets/MovieClip");
         trace(document);
-        var movieClip = new MovieClip(document.timeLines);
-        addChild(movieClip);
-        movieClip.gotoAndPlay(6, "场景 2");
-        movieClip.gotoAndStop("jump", "Scene 1");
+        mv = new MovieClip(document.timeLines);
+        addChild(mv);
+
+        var textField = new TextField();
+        textField.text = "下一帧";
+        addChild(textField);
+        textField.addEventListener(MouseEvent.CLICK, nextFrame);
+
+        var tfStop = new TextField();
+        tfStop.text = "停止";
+        tfStop.x = 100;
+        addChild(tfStop);
+        tfStop.addEventListener(MouseEvent.CLICK, stop);
+    }
+    
+    function stop(e:MouseEvent):Void
+    {
+        mv.stop();
+    }
+    
+    function nextFrame(e:MouseEvent):Void
+    {
+        mv.nextFrame();
     }
 }
