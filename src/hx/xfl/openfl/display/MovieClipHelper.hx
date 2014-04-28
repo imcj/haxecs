7d05fd567846c654a181b8ac1b8c41608019b9fe
xclmwc <xclmwc@aliyun.com>
2014-04-28 13:51:45 +0800

    空格替换Tab

--- src/hx/xfl/openfl/display/MovieClipHelper.hx
+++ src/hx/xfl/openfl/display/MovieClipHelper.hx
@@ -4,25 +4,25 @@ import flash.display.Sprite;
 
 class MovieClipHelper
 {
-	var frameIndexes:Map<String, Map<Int, Bool>>;
+    var frameIndexes:Map<String, Map<Int, Bool>>;
 
-	public function new(mc:Sprite)
-	{
-		frameIndexes = new Map();
-		frameIndexes.set("", new Map());
-	}
+    public function new(mc:Sprite)
+    {
+        frameIndexes = new Map();
+        frameIndexes.set("", new Map());
+    }
 
-	public function addFrame(frame_index:Int, ?scene:Null<String>):Void
-	{
-		if (scene == null)
-			scene = "";
+    public function addFrame(frame_index:Int, ?scene:Null<String>):Void
+    {
+        if (scene == null)
+            scene = "";
 
-		frameIndexes.get("").set(frame_index, true);
-	}
+        frameIndexes.get("").set(frame_index, true);
+    }
 
-	public function addFrames(frame_indexes:Array<Int>):Void
-	{
-		for (index in frame_indexes)
-			addFrame(index);
-	}
+    public function addFrames(frame_indexes:Array<Int>):Void
+    {
+        for (index in frame_indexes)
+            addFrame(index);
+    }
 }
