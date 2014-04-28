cc6b05f14d754d6de3dc9b54f5db97ea940143e9
xclmwc <xclmwc@aliyun.com>
2014-04-28 15:33:04 +0800

    添加DOMFontItem

---
+++ src/hx/xfl/DOMFontItem.hx
@@ -0,0 +1,22 @@
+package ;
+
+class DOMFontItem
+{
+    public var name:String;
+    public var itemID:String;
+    public var font:String;
+    public var size:Int;
+    public var id:Int;
+    public var embeddedCharacters:String;
+
+    public function new()
+    {
+        name = "";
+        itemID = "";
+        font = "";
+        size = 0;
+        id = 0;
+        embeddedCharacters = "";
+    }
+    
+}
