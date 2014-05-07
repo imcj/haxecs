22c016fa4d4575a89cba3faded42939dfaf57d91
xclmwc <xclmwc@aliyun.com>
2014-05-07 14:05:45 +0800

    添加windowsPath

---
+++ src/hx/WindowsPath.hx
@@ -0,0 +1,25 @@
+package hx;
+
+class WindowsPath implements IPath
+{
+
+    public function new()
+    {
+        
+    }
+    
+    public function join(a:String, b:Array<String>):String
+    {
+        return "";
+    }
+    
+    public function abspath(path:String):String
+    {
+        return "";
+    }
+    
+    public function dirname(path:String):String
+    {
+        return "";
+    }
+}
