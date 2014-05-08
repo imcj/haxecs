0ffbbe756adda0ddc6c6fffe2beb151ece9e992c
CJ <cj@imcj.me>
2014-05-08 14:54:49 +0800

    Null Function Pointer.

--- src/hx/Path.hx
+++ src/hx/Path.hx
@@ -6,8 +6,11 @@ class Path
 {
     static var path:IPath;
 
-    static public function __init__()
+    static var _initialized:Bool = false;
+    static public function initialize()
     {
+        if (_initialized)
+            return;
         #if (neko || cpp || php)
         var sys = Sys.systemName();
         
@@ -23,16 +26,19 @@ class Path
 
     static public function join(a:String, b:Array<String>):String
     {
+        initialize();
         return path.join(a, b);
     }
 
     static public function abspath(path:String):String
     {
+        initialize();
         return Path.path.abspath(path);
     }
 
     static public function dirname(path:String):String
     {
+        initialize();
         return Path.path.dirname(path);
     }
 }
