14143096c8068840b6a9e476eacc2b4fa8a87046
xclmwc <xclmwc@aliyun.com>
2014-06-04 14:35:44 +0800

    添加滤镜基础类

---
+++ src/hx/xfl/filter/Filter.hx
@@ -0,0 +1,24 @@
+﻿package hx.xfl.filter;
+
+import hx.xfl.filter.IFilter;
+
+class Filter implements IFilter
+{
+    public function new()
+    {
+    
+    }
+
+    public function parse():Void {
+        throw(new Error("Implement in subclasses!"));
+    }
+
+    public function clone():IFilter {
+        throw(new Error("Implement in subclasses!"));
+        return null;
+    }
+
+    public function toString():String {
+        return "[Filter]";
+    }
+}
---
+++ src/hx/xfl/filter/IFilter.hx
@@ -0,0 +1,8 @@
+﻿package hx.xfl.filter;
+
+interface IFilter
+{
+    function parse(data:Xml):Void;
+    function clone():IFilter;
+    function toString():String;
+}
